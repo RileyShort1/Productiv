298053e0755e73bea3a849f8fad8167080a43d11
Riley Short <riley.short@sjsu.edu>
2025-10-25 14:07:05 -0700

    ToDo list item class

---
+++ app/src/main/java/edu/sjsu/android/productiv/ToDoItem.java
@@ -0,0 +1,30 @@
+package edu.sjsu.android.productiv;
+
+import java.time.LocalDate;
+
+public class ToDoItem {
+    private String name;
+    private String description;
+    private LocalDate dueDate;
+    private int priority; // 1-5 priority level
+
+    public ToDoItem(String name, String description, LocalDate dueDate, int priority) {
+        this.name = name;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    public String getDescription() {
+        return this.description;
+    }
+    public LocalDate getDueDate() {
+        return this.dueDate;
+    }
+    public int getPriority() {
+        return this.priority;
+    }
+}
