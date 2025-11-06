119a3510d8587d4a9fd50f1a3a5e078dc1a57047
Riley Short <riley.short@sjsu.edu>
2025-11-05 20:35:24 -0800

    bug fix

--- app/src/main/java/edu/sjsu/android/productiv/todo_list.java
+++ app/src/main/java/edu/sjsu/android/productiv/todo_list.java
@@ -49,20 +49,19 @@ public class todo_list extends ListFragment {
         database.insert(new ToDoItem("CS147 HW", "do the lab assignment", LocalDate.now(), 2));
         database.insert(new ToDoItem("CS157C HW", "Complete Midterm", LocalDate.now(), 1));
 
-        getParentFragmentManager().setFragmentResultListener("requestKey", this, (key, bundle) -> {
-            ToDoItem newObject = (ToDoItem) bundle.getSerializable("result");
-            if (newObject != null) {
-                database.insert(newObject);
-                adapter.notifyDataSetChanged();
-            }
-        });
-
         todoItems = database.getAllToDoItems();
         todoItems.sort(ToDoItem::compareTo);
         // Create and set the adapter
         adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_list_item_1, todoItems);
         setListAdapter(adapter);
 
+        getParentFragmentManager().setFragmentResultListener("requestKey", this, (key, bundle) -> {
+            ToDoItem newObject = (ToDoItem) bundle.getSerializable("result");
+            if (newObject != null) {
+                database.insert(newObject);
+                addTodoItem(newObject);
+            }
+        });
     }
 
     public void onAddTaskButtonClick (View view) {
